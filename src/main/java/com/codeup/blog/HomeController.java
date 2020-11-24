0c7f732dbfd16043d1dbeaeccb12ed3f50e5dab9
RMKendall <ryan.morris.kendall@gmail.com>
2020-11-24 10:39:40 -0600

    Correct missing controller annotations at top of classes

--- src/main/java/com/codeup/blog/HomeController.java
+++ src/main/java/com/codeup/blog/HomeController.java
@@ -2,9 +2,11 @@ package com.codeup.blog;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+@Controller
 public class HomeController {
 
     @GetMapping("/")
@@ -12,6 +14,5 @@ public class HomeController {
     public String sayHello(){
         return "This is the landing page!";
     }
-
 
 }
--- src/main/java/com/codeup/blog/PostController.java
+++ src/main/java/com/codeup/blog/PostController.java
@@ -2,28 +2,35 @@ package com.codeup.blog;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+@Controller
 public class PostController {
 
     @GetMapping("/posts")
+    @ResponseBody
     public String allPosts(){
         return "posts";
     }
 
     @GetMapping("/posts/{id}")
+    @ResponseBody
     public String viewSelectedPost(@PathVariable int id){
         return "a post";
     }
 
     @GetMapping("/posts/create")
+    @ResponseBody
     public String viewCreatePostForm(){
         return "post create form";
     }
 
     @PostMapping("/posts/create")
+    @ResponseBody
     public String createNewPost(){
         return "new post";
     }
