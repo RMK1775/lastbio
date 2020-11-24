ea404e3afa5dda4d07f4fe5b08617dd08e9e5172
RMKendall <ryan.morris.kendall@gmail.com>
2020-11-24 11:37:46 -0600

    Remove generic verbiage and replace with variable assignments
    - return statements now include actual math operations

--- src/main/java/com/codeup/blog/MathController.java
+++ src/main/java/com/codeup/blog/MathController.java
@@ -2,32 +2,33 @@ package com.codeup.blog;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class MathController {
 
-    @GetMapping("/add/3/and/4")
+    @GetMapping("/add/{num1}/and/{num2}")
     @ResponseBody
-    public int addition() {
-        return 7;
+    public int addition(@PathVariable int num1, @PathVariable int num2) {
+        return num1 + num2;
     }
 
-    @GetMapping("/subtract/3/from/10")
+    @GetMapping("/subtract/{num1}/from/{num2}")
     @ResponseBody
-    public int subtraction() {
-        return 7;
+    public int subtraction(@PathVariable int num1, @PathVariable int num2) {
+        return num2 - num1;
     }
 
-    @GetMapping("/multiply/4/and/5")
+    @GetMapping("/multiply/{num1}/and/{num2}")
     @ResponseBody
-    public int multiplication() {
-        return 20;
+    public int multiplication(@PathVariable int num1, @PathVariable int num2) {
+        return num1 * num2;
     }
 
-    @GetMapping("/divide/6/and/3")
+    @GetMapping("/divide/{num1}/by/{num2}")
     @ResponseBody
-    public int division() {
-        return 2;
+    public int division(@PathVariable int num1, @PathVariable int num2) {
+        return num1 / num2;
     }
 }
