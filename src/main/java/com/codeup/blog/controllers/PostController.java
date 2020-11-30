49bca6053a7d19e7bf7554180900a970b4f084ed
RMKendall <ryan.morris.kendall@gmail.com>
2020-11-30 10:08:30 -0600

    Complete the MVC for Posts
    - Post model created
    - PostController created
    - html templates created for displaying either all posts or a single post

--- src/main/java/com/codeup/blog/controllers/PostController.java
+++ src/main/java/com/codeup/blog/controllers/PostController.java
@@ -1,37 +1,46 @@
-package com.codeup.blog;
+package com.codeup.blog.controllers;
 
+import com.codeup.blog.models.Post;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Controller
 public class PostController {
 
     @GetMapping("/posts")
-    @ResponseBody
-    public String allPosts(){
-        return "posts";
+    public String allPosts(Model model){
+        List<Post> postList = new ArrayList<>();
+        postList.add(new Post(0, "A Post", "This is a Post"));
+        postList.add(new Post(0, "Another Post", "This is another Post"));
+        model.addAttribute("posts", postList);
+        return "posts/index";
     }
 
     @GetMapping("/posts/{id}")
-    @ResponseBody
-    public String viewSelectedPost(@PathVariable long id){
-        return "a post";
+    public String viewSelectedPost(@PathVariable long id, Model model){
+        Post post = new Post(0, "Sample Post", "This would be the body for the selected post");
+        model.addAttribute("post", post);
+        return "posts/show";
     }
 
     @GetMapping("/posts/create")
     @ResponseBody
-    public String viewCreatePostForm(){
+    public String createNewPost(){
         return "post create form";
     }
 
     @PostMapping("/posts/create")
     @ResponseBody
-    public String createNewPost(){
-        return "new post";
+    public String savingNewPost(){
+        return "new post sent to database";
     }
 }
