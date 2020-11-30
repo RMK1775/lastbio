8224111686af0ac74e42e20166960693c1868691
RMKendall <ryan.morris.kendall@gmail.com>
2020-11-30 10:31:52 -0600

    Complete the dice roll portion of the Spring Views exercise

--- src/main/java/com/codeup/blog/controllers/DicerollController.java
+++ src/main/java/com/codeup/blog/controllers/DicerollController.java
@@ -10,15 +10,21 @@ import org.springframework.web.bind.annotation.*;
 public class DicerollController {
 
     @GetMapping("/roll-dice")
-    public String promptGuess(){
-        return("/roll-dice");
+    public String promptGuess() {
+        return ("/roll-dice");
     }
 
     @GetMapping("/roll-dice/{guess}")
-    public String returnResult(@PathVariable String guess, Model model){
-//        int userNum = (int) guess;
-        int rolledNum = (int) ((Math.random()*6)+1);
-        model.addAttribute("guess", guess);
-        return guess;
+    public String returnResult(@PathVariable String guess, Model model) {
+        int userNum = Integer.parseInt(guess);
+        int rolledNum = (int) ((Math.random() * 6) + 1);
+        String message = "Your guess was " + userNum + " and the number rolled was " + rolledNum + ".";
+        if (userNum == rolledNum) {
+            message += " Huzzah!";
+        } else {
+            message += " Bummer... try again?";
+        }
+        model.addAttribute("message", message);
+        return "/roll-dice";
     }
 }
