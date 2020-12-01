ff795f705c9b608abacf3dc67392c11f7605151e
RMKendall <ryan.morris.kendall@gmail.com>
2020-12-01 15:01:01 -0600

    Correct issues with dice roll exercise

--- src/main/java/com/codeup/blog/controllers/DicerollController.java
+++ src/main/java/com/codeup/blog/controllers/DicerollController.java
@@ -6,6 +6,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.concurrent.ThreadLocalRandom;
+
 @Controller
 public class DicerollController {
 
@@ -15,6 +17,19 @@ public class DicerollController {
     }
 
     @GetMapping("/roll-dice/{guess}")
+//    Walkthrough solution
+    public String rollDice(@PathVariable int guess, Model model) {
+        //Find a random number between 1 and 6.
+        int randomNum = ThreadLocalRandom.current().nextInt(1, 6 + 1);
+        //Compare the guess to the random number.
+        //Store if the number was guessed correctly in a model attribute.
+        model.addAttribute("randomNumber", randomNum);
+        model.addAttribute("myGuess", guess);
+        model.addAttribute("isCorrectGuess", guess == randomNum);
+
+        return "/roll-dice";
+    }
+/*Needs to be reworked along with corresponding HTML
     public String returnResult(@PathVariable String guess, Model model) {
         int userNum = Integer.parseInt(guess);
         int rolledNum = (int) ((Math.random() * 6) + 1);
@@ -27,4 +42,5 @@ public class DicerollController {
         model.addAttribute("message", message);
         return "/roll-dice";
     }
+*/
 }
